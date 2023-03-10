04c2b5097e64742cde09b49a27983d315ce94aae
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-03-10 20:58:23 +0100

    uodate registerForm

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,23 +1,39 @@
-import { Form, Label } from "./RegisterForm.styled";
+import { useDispatch } from 'react-redux';
+import { register } from 'redux/auth/operations';
+import { Form, Label } from './RegisterForm.styled';
 
 
 export const RegisterForm = () => {
-    return (
-    <Form>
-        <Label>
-          Username
-          <input type="text" name="name" />
-        </Label>
-        <Label type="email" name="email">
-          Email
-          <input />
-        </Label>
-        <Label>
-          Password
-          <input type="password" name="password"/>
-        </Label>
-        <button type="submit"> Register</button>
-      </Form>
-      )
-  
+  const dispatch = useDispatch();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    dispatch(
+      register({
+        name: form.elements.name.value,
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
+    );
+    form.reset()
+  };
+
+  return (
+    <Form onSubmit={handleSubmit} autoComplete="off">
+      <Label>
+        Username
+        <input type="text" name="name" />
+      </Label>
+      <Label type="email" name="email">
+        Email
+        <input />
+      </Label>
+      <Label>
+        Password
+        <input type="password" name="password" />
+      </Label>
+      <button type="submit"> Register</button>
+    </Form>
+  );
 };
