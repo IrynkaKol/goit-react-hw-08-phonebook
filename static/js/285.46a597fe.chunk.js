d709f81c88ade8d5453be7a4d08fc17e817d1341
IrynkaKol <106806837+IrynkaKol@users.noreply.github.com>
2023-03-10 16:43:34 +0000

    Deploying to gh-pages from @ IrynkaKol/goit-react-hw-08-phonebook@b46074cb40ab3fe5705c2656031f4c0eb6e84e3c 🚀

--- static/js/285.46a597fe.chunk.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[285],{3246:function(){},285:function(e,t,r){"use strict";r.r(t),r.d(t,{default:function(){return c}});var n=r(3246),i=r(184);function c(){return(0,i.jsxs)("div",{children:[(0,i.jsx)("title",{children:"Registration"}),(0,i.jsx)(n.RegisterForm,{})]})}}}]);
-//# sourceMappingURL=285.46a597fe.chunk.js.map
