cf79279dcadfb01975b7ab10fc20c81c4bf30ee5
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-03-11 07:30:31 +0100

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
-import { useEffect, lazy, Suspense } from 'react';
-import { useDispatch } from 'react-redux';
+import { lazy, Suspense } from 'react';
+//import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 //import { Navigation } from '../Navigation/Navigation';
 //import { PrivateRoute } from '../PrivateRoute';
 //import { RestrictedRoute } from '../RestrictedRoute';
 
 //import { useAuth } from 'hooks';
-import { refreshUser } from 'redux/auth/operations';
+//import { refreshUser } from 'redux/auth/operations';
 import { Layout } from 'components/Layout';
 
 const HomePage = lazy(() => import('../../pages/Home'));
@@ -15,12 +15,12 @@ const LoginPage = lazy(() => import('../../pages/Login'));
 const ContactsPage = lazy(() => import('../../pages/Contacts'));
 
 export const App = () => {
-  const dispatch = useDispatch();
-  //const { isRefreshing } = useAuth();
+  /*const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
     dispatch(refreshUser());
-  }, [dispatch]);
+  }, [dispatch]);*/
 
   return (
     <Suspense>
