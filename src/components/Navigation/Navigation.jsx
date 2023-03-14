e72fea509f878a391bea064da49c936c706df8fe
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-03-14 22:25:24 +0100

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { useAuth } from 'hooks';
 import { PhonebookLogo } from 'phonebook-logo';
-import { Flex, Box } from '@chakra-ui/react';
+import { Flex, Box, Heading } from '@chakra-ui/react';
 
 export const Navigation = () => {
   const { isLoggedIn } = useAuth();
@@ -16,6 +16,11 @@ export const Navigation = () => {
         <NavLink to="/">
           <Box display="flex" mt="2" alignItems="center">
             <PhonebookLogo />
+            <Heading as="h1"
+            fontWeight="300"
+            fontSize="24px"
+            
+            >Phonebook</Heading>
           </Box>
         </NavLink>
         {isLoggedIn && <NavLink style={{color: 'blue'}} to="/contacts">Contacts</NavLink>}
