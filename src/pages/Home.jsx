6cf5dfdf3cdd3514633722a726a6231237f067b6
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-03-15 23:40:34 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,19 @@
-import { Box, Center, VStack, Heading } from '@chakra-ui/react';
+import { Box, Center, VStack, Heading} from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <Box>
       <Center>
         <VStack>
-           <Heading
+          <Heading
             as="h1"
             backgroundImage="conic-gradient(rgb(85, 60, 154), rgb(238, 75, 43), rgb(0, 194, 203), rgb(85, 60, 154))"
             color="transparent"
             backgroundClip="text"
-            textAlign = 'center'
+            textAlign="center"
           >
             Create your own Phonebook
+            
           </Heading>
         </VStack>
       </Center>
